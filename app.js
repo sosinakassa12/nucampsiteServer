64b709d00a24f76267b5618e91d3b86e75cde1fe
Sosina <sosinakassa12@gmail.com>
2025-11-15 10:56:09 -0600

    CORS

--- app.js
+++ app.js
@@ -1,75 +1,78 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 const passport = require('passport');
+const mongoose = require('mongoose');
 const config = require('./config');
+const cors = require('./routes/cors'); // make sure cors.js exports corsWithOptions
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Routers
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
 
-const mongoose = require('mongoose');
+const app = express();
 
-
-// const url = 'mongodb://127.0.0.1:27017/nucampsite';
+// MongoDB connection
 const url = config.mongoUrl;
-const connect = mongoose.connect(url, {});
-
-connect.then(() => console.log('Connected correctly to server'),
-  err => console.log(err)
-);
-
-var app = express();
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected correctly to server'))
+    .catch(err => console.log(err));
 
 // Secure traffic only
 app.all('*', (req, res, next) => {
-    if (req.secure) {
-        return next();
-    } else {
-        console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
-        res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
-    }
+    if (req.secure) return next();
+    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
 });
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Enable CORS for whitelist
+app.use(cors.corsWithOptions);
+
+// Initialize Passport
 app.use(passport.initialize());
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routers
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// Catch 404
+app.use((req, res, next) => next(createError(404)));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Error handler
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+
+    // If request expects HTML, render error page
+    if (req.accepts('html')) {
+        res.render('error');
+    } else {
+        // Otherwise send JSON (useful for API testing)
+        res.json({ error: err.message });
+    }
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,69 +2,77 @@ const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require('../authenticate');
+const cors = require('./cors');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    User.find()
-    .then(users => res.status(200).json(users))
-    .catch(err => next(err))
-});
+router.route('/')
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+        User.find()
+        .then(users => res.status(200).json(users))
+        .catch(err => next(err));
+    }
+);
 
-router.post('/signup', (req, res) => {
+router.route('/signup')
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.post(cors.corsWithOptions, (req, res) => {
     const user = new User({ username: req.body.username });
 
     User.register(user, req.body.password)
         .then(registeredUser => {
-            if (req.body.firstname) {
-                registeredUser.firstname = req.body.firstname;
-            }
-            if (req.body.lastname) {
-                registeredUser.lastname = req.body.lastname;
-            }
+            if (req.body.firstname) registeredUser.firstname = req.body.firstname;
+            if (req.body.lastname) registeredUser.lastname = req.body.lastname;
             return registeredUser.save();
         })
         .then(() => {
             passport.authenticate('local')(req, res, () => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json({ success: true, status: 'Registration Successful!' });
+                res.status(200).json({
+                    success: true,
+                    status: 'Registration Successful!'
+                });
             });
         })
         .catch(err => {
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({ err: err });
+            res.status(500).json({ err: err });
         });
 });
 
-router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
-    const token = authenticate.getToken({_id: req.user._id});
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({success: true, token: token, status: 'You are successfully logged in!'});
-});
+router.route('/login')
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.post(cors.corsWithOptions, passport.authenticate('local', { session: false }),
+    (req, res) => {
+        const token = authenticate.getToken({ _id: req.user._id });
+        res.status(200).json({
+            success: true,
+            token: token,
+            status: 'You are successfully logged in!'
+        });
+    }
+);
 
-router.get('/logout', (req, res, next) => {  
-    if (req.user) {  // Check if user is authenticated
-        req.logout((err) => {  // Passport.js logout
-            if (err) {
-                return next(err);
-            }
-            req.session.destroy((err) => {  // Destroy session
-                if (err) {
-                    return next(err);
-                }
-                res.clearCookie('session-id');  // Clear session cookie
+router.route('/logout')
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.corsWithOptions, (req, res, next) => {
+    if (req.user) {
+        req.logout(err => {
+            if (err) return next(err);
+            req.session.destroy(err => {
+                if (err) return next(err);
+                res.clearCookie('session-id');
                 res.redirect('/');
             });
         });
-    } else {  
+    } else {
         const err = new Error('You are not logged in!');
         err.status = 401;
         return next(err);
-    }  
+    }
 });
 
 module.exports = router;
